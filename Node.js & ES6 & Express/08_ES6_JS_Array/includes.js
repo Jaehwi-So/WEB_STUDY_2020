2399b7703d257b87ccaeae0fa60cfe50d6daac7f
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-09-29 11:37:59 +0900

    배열 조작 : includes(), map(), some()

---
+++ Node.js & ES6 & Express/08_ES6_JS_Array/includes.js
@@ -0,0 +1,9 @@
+`use strict`
+const arr = ['red', 'yellow', 'blue'];
+
+//includes : 해당 엘리멘트가 존재하면 true를 반환
+const res = arr.includes('red');
+const res2 = arr.includes('green');
+
+console.log(res);
+console.log(res2);
