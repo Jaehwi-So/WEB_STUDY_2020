977cc91a8a4e1d1eab74c53e5e88e24e71223de7
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-10-15 04:33:17 +0900

    데이터베이스 생성

---
+++ DBMS_MySQL/04_CREATE_DATABASE_SCHEMA.sql
@@ -0,0 +1,8 @@
+-- # 데이터베이스 생성
+CREATE DATABASE test;
+CREATE DATABASE `test2`;
+CREATE SCHEMA test3`;
+CREATE SCHEMA `test4`;
+
+-- 한글 사용을 위해서 utf-8 인코딩 설정
+CREATE SCHEMA `test1` DEFAULT CHARACTER SET utf8;
